e4daf75a4991372c9293db433837af1251313da3
Robert McKenney <robert@mckenney.ca>
2018-04-30 15:35:42 -0400

    Base config
    Vue-cli 3 + customized prettier and eslint rules for clean auto formatting on save.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,20 @@
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ]
+  extends: ['eslint:recommended', 'plugin:vue/essential', '@vue/standard'],
+  plugins: ['vue', 'prettier'],
+  rules: {
+    'import/no-unresolved': 0,
+    'import/no-unassigned-import': 0,
+    semi: ['error', 'never'],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'no-console': 'off',
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'always',
+        named: 'always',
+        asyncArrow: 'always'
+      }
+    ]
+  }
 }
